295fa6a9570c42cc98b63536527ec98853260f8a
Shiz <141632242+Shazil0204@users.noreply.github.com>
2024-10-09 10:48:57 +0200

    auth screens

---
+++ Mobile/app/(auth)/team.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const team = () => {
+  return (
+    <View>
+      <Text>team</Text>
+    </View>
+  );
+};
+
+export default team;
+
+const styles = StyleSheet.create({});
